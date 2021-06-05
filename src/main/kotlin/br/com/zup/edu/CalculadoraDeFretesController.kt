5c11b488fd9665833420ef574565bf3fbbc394de
Guilherme Souza <guilhermejcamargos@gmail.com>
2021-06-04 21:14:23 -0300

    Tratamento de erros

--- src/main/kotlin/br/com/zup/edu/CalculadoraDeFretesController.kt
+++ src/main/kotlin/br/com/zup/edu/CalculadoraDeFretesController.kt
@@ -1,8 +1,13 @@
 package br.com.zup.edu
 
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
+import io.grpc.protobuf.StatusProto
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.QueryValue
+import io.micronaut.http.exceptions.HttpStatusException
 
 @Controller
 class CalculadoraDeFretesController(val gRpcClient: FretesServiceGrpc.FretesServiceBlockingStub) {
@@ -14,9 +19,33 @@ class CalculadoraDeFretesController(val gRpcClient: FretesServiceGrpc.FretesServ
             .setCep(cep)
             .build()
 
-        val response = gRpcClient.calculaFrete(request)
+        try {
+            val response = gRpcClient.calculaFrete(request)
+            return FreteResponse(cep = response.cep, valor = response.valor)
+        } catch (e: StatusRuntimeException) {
 
-        return FreteResponse(cep = response.cep, valor = response.valor)
+            val description = e.status.description
+            val statusCode = e.status.code
+            if(statusCode == Status.Code.INVALID_ARGUMENT){
+                throw HttpStatusException(HttpStatus.BAD_REQUEST, description)
+            }
+
+            if(statusCode == Status.Code.PERMISSION_DENIED){
+                val statusProto = StatusProto.fromThrowable(e)
+                if(statusProto == null){
+                    throw HttpStatusException(HttpStatus.FORBIDDEN, description)
+                }
+                val anyDetails = statusProto.detailsList.get(0)
+                val errorDetails = anyDetails.unpack(ErrorDetails::class.java)
+
+                throw HttpStatusException(HttpStatus.FORBIDDEN, "${errorDetails.code}: ${errorDetails.message}")
+            }
+
+
+            // caso contrário
+            throw HttpStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.message)
+        }
+
     }
 
     class FreteResponse(val cep: String, val valor: Double) {
